7b068961b3742965bcffadc4c988e4169769c991
WikarNotAvailable <wikar8998@gmail.com>
2022-11-16 22:39:18 +0100

    removed unnecessary comment

--- StringReverse/Controllers/StringReversionController.cs
+++ StringReverse/Controllers/StringReversionController.cs
@@ -7,7 +7,7 @@ namespace StringReverse.Controllers
     public class StringReversionController : Controller
     {
         private readonly StringReversionModel _reversionModel = new StringReversionModel();
-        // GET: StringReversionController
+
         public ActionResult Index()
         {
             return View();
