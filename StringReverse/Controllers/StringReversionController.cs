02988e2ce05ac88e43e222a937776cfd82027e31
WikarNotAvailable <wikar8998@gmail.com>
2022-11-16 22:04:22 +0100

    added views

--- StringReverse/Controllers/StringReversionController.cs
+++ StringReverse/Controllers/StringReversionController.cs
@@ -13,6 +13,21 @@ namespace StringReverse.Controllers
             return View();
         }
 
-       
+        public ActionResult ShowInputPanel()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Reverse()
+        {
+            var stringToReverse = Request.Form["stringToReverse"];
+            var reversedString = _reversionModel.ReverseString(stringToReverse);
+
+            ViewBag.ReversedString = reversedString;
+            ViewBag.OldString = stringToReverse;
+            return View();
+        }
     }
 }
