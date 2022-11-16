17adf59dd5b824b6caaf7cc03e2e15c6f5849905
WikarNotAvailable <wikar8998@gmail.com>
2022-11-16 22:26:40 +0100

    implemented recurive method reversing string

--- StringReverse/Models/StringReversionModel.cs
+++ StringReverse/Models/StringReversionModel.cs
@@ -6,8 +6,10 @@
 
         public string ReverseString(string stringToReverse)
         {
-            //Reversion - to implement
-            return stringToReverse;
+            if (stringToReverse.Length > 0)
+                return stringToReverse[stringToReverse.Length - 1] + ReverseString(stringToReverse.Substring(0, stringToReverse.Length - 1));
+            else
+                return stringToReverse;
         }
     }
 }
