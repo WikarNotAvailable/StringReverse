16c4812cac6cb4c1dde7707917ec9e6133d23222
WikarNotAvailable <wikar8998@gmail.com>
2022-11-16 19:42:46 +0100

    setup of project

---
+++ StringReverse/Controllers/StringReversionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StringReverse.Models;
+
+namespace StringReverse.Controllers
+{
+    public class StringReversionController : Controller
+    {
+        private readonly StringReversionModel _reversionModel = new StringReversionModel();
+        // GET: StringReversionController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ StringReverse/Models/StringReversionModel.cs
@@ -0,0 +1,13 @@
+﻿namespace StringReverse.Models
+{
+    public class StringReversionModel
+    {
+        public StringReversionModel() { }
+
+        public string ReverseString(string stringToReverse)
+        {
+            //Reversion - to implement
+            return stringToReverse;
+        }
+    }
+}
